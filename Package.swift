d1ce72b566aa7fcea511fc3e7a3e53b44f6db272
Sam Deane <sam@elegantchaos.com>
2026-04-16 16:15:15 +0100

    Added a CI workflow

--- Package.swift
+++ Package.swift
@@ -3,44 +3,47 @@
 import PackageDescription
 
 let package = Package(
-    name: "Icons",
+  name: "Icons",
 
-    platforms: [
-      .macOS("26.0"), .iOS("26.0"), .tvOS("26.0"),
-    ],
+  platforms: [
+    .macOS("26.0"), .iOS("26.0"), .tvOS("26.0"),
+  ],
 
-    products: [
-        .library(
-            name: "Icons",
-            targets: ["Icons"]
-        ),
-    ],
-    
-    targets: [
-        .target(
-            name: "Icons"
-        ),
-        .testTarget(
-            name: "IconsTests",
-            dependencies: ["Icons"]
-        ),
-    ]
+  products: [
+    .library(
+      name: "Icons",
+      targets: ["Icons"]
+    )
+  ],
+
+  dependencies: [
+    .package(url: "https://github.com/elegantchaos/ActionBuilderPlugin.git", from: "2.1.4")
+  ],
+
+  targets: [
+    .target(
+      name: "Icons"
+    ),
+    .testTarget(
+      name: "IconsTests",
+      dependencies: ["Icons"]
+    ),
+  ]
 )
 
 for target in package.targets {
   switch target.type {
-    case .regular, .test:
-      var settings = target.swiftSettings ?? []
-      settings.append(contentsOf: [
-        .defaultIsolation(MainActor.self),
-        .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
-        .enableUpcomingFeature("InferIsolatedConformances"),
-        .enableExperimentalFeature("SendableProhibitsMainActorInference"),
-      ])
-      target.swiftSettings = settings
-    default:
-      continue
+  case .regular, .test:
+    var settings = target.swiftSettings ?? []
+    settings.append(contentsOf: [
+      .defaultIsolation(MainActor.self),
+      .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
+      .enableUpcomingFeature("InferIsolatedConformances"),
+      .enableExperimentalFeature("SendableProhibitsMainActorInference"),
+    ])
+    target.swiftSettings = settings
+  default:
+    continue
   }
 
 }
-
